b107e88d87d0cb7ba1077efbe7406a91e5234333
Akhilesh Mourya <akhileshm@neuronsolutions.com>
2015-06-18 16:49:02 +0530

    June18 Commit

---
+++ MyGitDemo/MyGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGitDemo
+//
+//  Created by Neuron Mac on 18/06/15.
+//  Copyright (c) 2015 Neuron Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGitDemo/MyGitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGitDemo
+//
+//  Created by Neuron Mac on 18/06/15.
+//  Copyright (c) 2015 Neuron Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
